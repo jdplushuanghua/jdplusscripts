d07c44bf6fe4d27d17ef00044677a8ed0a0f6975
wq446879031 <47649695+wq446879031@users.noreply.github.com>
2020-12-28 20:34:57 +0800

    Add files via upload

--- activity/jd_split.js
+++ activity/jd_split.js
@@ -44,7 +44,14 @@ if ($.isNode()) {
   cookiesArr = cookiesArr.filter(item => item !== "" && item !== null && item !== undefined);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-$.newShareCodes = [`P04z54XCjVUnIaW5nJcXCCyoR8C6p8txXBH`, 'P04z54XCjVUnIaW5m9cZ2T6jChKki0Hfndla5k', 'P04z54XCjVUnIaW5u2ak7ZCdan1BT0NlbBGZ1-rnMYj', 'P04z54XCjVUnIaW5m9cZ2ariXVJwI64DaVTNXQ'];
+$.newShareCodes = [
+  "P04z54XCjVUnIaW5m9cZzKCojQL8_W11FcgXg",
+  "P04z54XCjVUnIaW5m9cZ2X92X8bwAZnvz8wedM",
+  "P04z54XCjVUnIaW5m9cZyinnjUR8lukiHWGBA",
+  "P04z54XCjVUnIaW5m9cZ2f62H0ZwCGuj3foPxM",
+  "P04z54XCjVUnIaW5jcKCmjx3HpPySvX",
+  "P04z54XCjVUnIaW5m9cZxeduA027UVt3xQGog",
+];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
